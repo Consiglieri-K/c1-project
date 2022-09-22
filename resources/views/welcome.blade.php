4df647a493728ade4aa4d37b77814eb2d1698196
KC <93526857+Consiglieri-K@users.noreply.github.com>
2022-09-22 15:56:58 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="biggestWrapper">
+        <div class="bigWrapper">
+            <header>
+                <div class="headerWrapper">
+                    <div class="logo">
+                        <a><img src="img/curio-logo-1.png" alt="curio logo"></a>
+                    </div>
+                    <div class="navBar">
+                        <nav>
+                            <a href="#vakken">Vakken</a>
+                            <a href="#rooster">Rooster</a>
+                            <a href="">Versneld traject</a>
+                            <a href="">Docenten</a>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <div class="scroll-bg">
+                    <div class="scroll-div">
+                        <div class="scroll-object">
+                            <h1 id="vakken">Vakken</h1>
+                        </div>
+                        <div class="scroll-object">
+                            <div class="scroll-text">
+                                <h2>NATIVE</h2>
+                                <p>Bij native development leer je hoe je programma's gaat maken voor Windows systemen. Je leert applicaties maken door middel van onder andere c# en python.</p>
+                            </div>
+                            <div class="scroll-img">
+                                <a><img src="img/vslogo.png" alt="vsstudio"></a>
+                                <a><img src="img/python.png" alt="python"></a>
+                            </div>
+                        </div>
+                        <div class="scroll-object">
+                            <div class="scroll-text">
+                                <h2>WEB</h2>
+                                <p>Tijdens de Web development lessen leer je websites en webapplicaties maken. Je gaat te werk met html,css, java en php en daarnaast werk je ook met databases en de sql taal.</p>
+                            </div>
+                            <div class="scroll-img">
+                                <a><img src="img/php.png" alt="php-logo"></a>
+                                <a><img src="img/html.png" alt="html"></a>
+                                <a><img src="img/sql.png" alt="sql"></a>
+                                <a><img src="img/css.png" alt="css"></a>
+                            </div>
+                        </div>
+                        <div class="scroll-object">
+                            <div class="scroll-text">
+                                <h2>PRA</h2>
+                                <p>Bij praktijkwerk gebruik je de kennis die je bij de Native en Web lessen hebt opgedaan om projecten te gaan maken. Denk hierbij aan applicaties en webapps of een combinatie ervan.</p>
+                            </div>
+                            <div class="scroll-img">
+                                <a><img src="img/php.png" alt="php-logo"></a>
+                                <a><img src="img/html.png" alt="html"></a>
+                                <a><img src="img/sql.png" alt="sql"></a><a><img src="img/vslogo.png" alt="vsstudio"></a>
+                                <a><img src="img/python.png" alt="python"></a>
+                                <a><img src="img/css.png" alt="css"></a>
+                            </div>
+                        </div>
+                        <div class="scroll-object">
+                            <div class="scroll-text">
+                                <h2>ALGO</h2>
+                                <p>Algo gaat over de algorithmes en het denkwerk dat achter de code zit. Om te kunnen coderen moet je ook de gedachtegang leren van het vak. We leren dit in de vorm van PHP.</p>
+                            </div>
+                            <div class="scroll-img">
+                                <a><img src="img/php.png" alt="php-logo"></a>
+                                <a><img src="img/mindset.png" alt="php-logo"></a>
+                            </div>
+                        </div>
+                        <div class="scroll-object">
+                            <div class="scroll-text">
+                                <h2>PRO</h2>
+                                <p>Het vak PRO gaat over de aanpak van software projecten en hoe het er aan toe gaat buiten het coderen zelf.</p>
+                            </div>
+                            <div class="scroll-img">
+                                <a><img src="img/php.png" alt="php-logo"></a>
+                                <a><img src="img/mindset.png" alt="php-logo"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="scroll-div">
+                        <div class="scroll-object">
+                            <h1 id="rooster">Rooster</h1>
+                        </div>
+                        <div class="scroll-object">
+                            <div class="scroll-text">
+                                <h2></h2>
+                                <p></p>
+                            </div>
+                            <div class="rooster-img">
+                                <a><img src="img/vslogo.png" alt="vsstudio"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+    <footer>
+        <div class="bigWrapper">
+           <div class="footerBox">
+                <li>&#169; Krijn Consiglieri 2022</li>
+                <div class="ul">
+                    <li>Vakken</li>
+                    <li>Rooster</li>
+                    <li>Versneld traject</li>
+                    <li>Docenten</li>
+                </div>
+           </div>   
+        </div>   
+    </footer>
+</body>
+</html>
